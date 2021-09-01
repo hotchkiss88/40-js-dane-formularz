15acea850e4155b86420e0bb8ffde9139267eaf3
hotchkiss88 <al-sowa@hotmail.com>
2021-09-01 22:08:53 +0200

    skonczony CSS

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-let objectForm = document.getElementById("form");
+// let objectForm = document.getElementById("form");
 let objectOne = document.getElementById("one");
-let objectTwo = document.getElementById("two");
-let objectThree = document.getElementById("three");
+// let objectTwo = document.getElementById("two");
+
+// le// let objectThree = document.getElementById("three");
 let objectName = document.getElementById("name");
 let objectSurname = document.getElementById("surname");
-let objectPhones = document.getElementById("phone");
+// let objectPhones = document.getElementById("phone");
 let objectKnop = document.getElementById("knop");
 
 objectKnop.onclick = function() {
 
     if (objectOne.value.length > 0) {
-
+        document.getElementById("one").outerHTML;
     }
-
-
-
 }
