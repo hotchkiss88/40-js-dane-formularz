537b183ba516bcdf0883b929bf53fbb72a46572b
hotchkiss88 <al-sowa@hotmail.com>
2021-09-01 17:39:35 +0200

    40-js-dane-formularz

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let objectForm = document.getElementById("form");
+let objectOne = document.getElementById("one");
+let objectTwo = document.getElementById("two");
+let objectThree = document.getElementById("three");
+let objectName = document.getElementById("name");
+let objectSurname = document.getElementById("surname");
+let objectPhones = document.getElementById("phone");
+let objectKnop = document.getElementById("knop");
+
+objectKnop.onclick = function() {
+
+    if (objectOne.value.length > 0) {
+
+    }
+
+
+
+}
