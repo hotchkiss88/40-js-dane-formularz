600b43187e39a6ce311d54896e780a5d273dcbad
hotchkiss88 <al-sowa@hotmail.com>
2021-09-02 12:15:30 +0200

    poprawka

--- js/main.js
+++ js/main.js
@@ -4,12 +4,27 @@ let objectTwo = document.getElementById("two");
 let objectThree = document.getElementById("three");
 let objectName = document.getElementById("name");
 let objectSurname = document.getElementById("surname");
-let objectPhones = document.getElementById("phone");
+let objectPhone = document.getElementById("phone");
 let objectKnop = document.getElementById("knop");
 
 objectKnop.onclick = function() {
 
     if (objectOne.value.length > 0) {
+        objectName.textContent = objectOne.value;
+        return objectName;
+    }
 
+    if (objectTwo.value.length > 0) {
+        objectSurname.textContent = objectTwo.value;
+        return objectSurname;
+    }
+
+    if (objectOne.value.length > 0) {
+        objectName.textContent = objectOne.value;
+        return objectName;
+    }
+    if (objectThree.value.length > 0) {
+        objectPhone.textContent = objectThree.value;
+        return objectPhone;
     }
 }
